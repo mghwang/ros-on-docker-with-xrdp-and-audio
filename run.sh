e0b75a18e0deb452ebb68d2cc7fd6c1d3a454ad9
황명규(Myunggyu Hwang)/Biz플랫폼개발팀/SKP <mghwang@sk.com>
2023-09-14 10:00:39 +0900

    container name change in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name ros-on-docker-with-xrdp-from-danielguerra --shm-size 1g -p 3389:3389 $(cat ./IMAGE_TAG)
+docker run -d --name ros-on-docker-with-xrdp-and-audio --shm-size 1g -p 3389:3389 $(cat ./IMAGE_TAG)
