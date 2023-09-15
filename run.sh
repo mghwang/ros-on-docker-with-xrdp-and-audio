d894f15f5d25aae8751d16523156d28dfb17b4ad
황명규(Myunggyu Hwang)/Biz플랫폼개발팀/SKP <mghwang@sk.com>
2023-09-15 17:42:33 +0900

    added rocker support (not tested yet)

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run -d --name ros-on-docker-with-xrdp-and-audio --shm-size 1g -p 3389:3389 $(cat ./IMAGE_TAG)
+if which rocker > /dev/null; then
+  rocker --nvidia --x11 --pulse --name ros-on-docker-with-xrdp-and-audio --shm-size 1g --port 3389:3389 $(cat ./IMAGE_TAG)
+else
+  docker run -d --name ros-on-docker-with-xrdp-and-audio --shm-size 1g -p 3389:3389 $(cat ./IMAGE_TAG)
+fi
