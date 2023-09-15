524b6ef39bb2366a1fc9cfefa0354764e672d75c
mghwang <mghwang@users.noreply.github.com>
2023-09-15 22:28:29 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if which rocker > /dev/null; then
-  rocker --nvidia --x11 --pulse --name ros-on-docker-with-xrdp-and-audio --shm-size 1g --port 3389:3389 $(cat ./IMAGE_TAG)
+  rocker --nvidia --x11 --pulse --name ros-on-docker-with-xrdp-and-audio --port 3389:3389 $(cat ./IMAGE_TAG)
 else
   docker run -d --name ros-on-docker-with-xrdp-and-audio --shm-size 1g -p 3389:3389 $(cat ./IMAGE_TAG)
 fi
